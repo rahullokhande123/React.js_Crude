1ef84b5e0a41a72aec219f993ad6802cf3954878
Rahul Lokhande <rlokhande06244@gmail.com>
2024-08-20 13:48:33 +0530

    Changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -22,14 +22,45 @@ const Home=()=>{
 
       <div id="homeCart">
 
-      <Card style={{ width: '12rem' }}>
-      <Card.Img variant="top" src="/public/python.jpeg" style={{width:"190px"}}/>
+      <Card style={{ width: '10rem' }}>
+      <Card.Img variant="top" src="/public/python.jpeg" style={{width:"100%"}}/>
       <Card.Body>
         <Card.Title>Python</Card.Title>
         <Card.Text>
           Fullstack Python 
         </Card.Text>
-        <Button variant="primary">Purchase</Button>
+        <Card.Text>
+          <b>Fees : 45,000 Rs.</b>
+        </Card.Text>
+        <Button variant="dark">Purchase</Button>
+      </Card.Body>
+    </Card>
+
+    <Card style={{ width: '10rem' }}>
+      <Card.Img variant="top" src="/public/python.jpeg" style={{width:"100%"}}/>
+      <Card.Body>
+        <Card.Title>Java</Card.Title>
+        <Card.Text>
+          Fullstack Java 
+        </Card.Text>
+        <Card.Text>
+          <b>Fees : 50,000 Rs.</b>
+        </Card.Text>
+        <Button variant="dark">Purchase</Button>
+      </Card.Body>
+    </Card>
+
+    <Card style={{ width: '10rem' }}>
+      <Card.Img variant="top" src="/public/python.jpeg" style={{width:"100%"}}/>
+      <Card.Body>
+        <Card.Title>MERN Stack</Card.Title>
+        <Card.Text>
+        MongoDB, Express, React, and Node. 
+        </Card.Text>
+        <Card.Text>
+          <b>Fees : 42,000 Rs.</b>
+        </Card.Text>
+        <Button variant="dark">Purchase</Button>
       </Card.Body>
     </Card>
       </div>
